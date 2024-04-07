74f2a8d102af726ef53642994f467369d15cddb9
hipolis <hipolis@163.com>
2024-04-07 22:29:55 +0800

    1.Update test

---
+++ src/main/java/com/hipolis/edu/manage/EduManageStarter.java
@@ -0,0 +1,18 @@
+package com.hipolis.edu.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @className: EduManageStarter
+ * @description:
+ * @author: hipolis
+ * @create: 2024-04-07 22:01
+ **/
+@SpringBootApplication
+public class EduManageStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduManageStarter.class, args);
+    }
+}
